fec76acfc0748cafb6d15ab128468621a45ca985
Fatou Diagne <f.m.diagne1@gmail.com>
2020-07-08 16:08:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,13 @@
-def speak_to_grandma ()
+def speak_to_grandma (phrase)
+  if phrase == .upcase
 end
 
-if speak_to_grandma == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
-elsif speak_to_grandma == "Hi Nana, how are you?"
-  puts "HUH?! SPEAK UP, SONNY!"
-else speak_to_grandma == .upcase
-  puts "NO, NOT SINCE 1938!"
-end
+# if speak_to_grandma == "I LOVE YOU GRANDMA!"
+#   puts "I LOVE YOU TOO PUMPKIN!"
+# elsif speak_to_grandma == "Hi Nana, how are you?"
+#   puts "HUH?! SPEAK UP, SONNY!"
+# else speak_to_grandma == .upcase
+#   puts "NO, NOT SINCE 1938!"
+# end
   
   
