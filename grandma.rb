1a34b387bd0e046e5dad6f0ba4b97e0ace4b415c
Fatou Diagne <f.m.diagne1@gmail.com>
2020-07-08 16:24:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma (phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
- elsif speak_to_grandma == "Hi Nana, how are you?"
+ elsif phrase == "Hi Nana, how are you?"
   puts "HUH?! SPEAK UP, SONNY!"
 else #speak_to_grandma == .upcase
   puts "NO, NOT SINCE 1938!"
